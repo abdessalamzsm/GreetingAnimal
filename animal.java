67cc0a579a61805d9905921314f8e28486f633ba
Abdessalam El Ghoul <aelghoul@SMB.SQLI.COM>
2022-03-14 14:47:12 +0100

    Bird greeting added

--- animal.java
+++ animal.java
@@ -3,9 +3,22 @@ class animal{
     {
         return "Hello!";
     }
+
+    public static String animalBirdGreeting()
+    {
+        return "tweet";
+    }
+
+
     public static void main(String[] args) {
+        String firstArgument = args[0];
         String output = "";
-        output = animalDefaultGreeting();
+        if(firstArgument.equalsIgnoreCase("Bird"))
+        {
+            output = animalBirdGreeting();
+        }
+        else
+            output = animalDefaultGreeting();
         System.out.println(output);
     }
 }
